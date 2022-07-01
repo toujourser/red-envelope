5c9d1938804dfc35b3054f16ff7d2a275a4e14b2
toujourser <toujourser@gmail.com>
2022-07-01 10:59:16 +0800

    update: 完善细节

--- brun/main.go
+++ brun/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"github.com/tietang/props/ini"
-	"github.com/tietang/props/kvs"
+	"net/http"
 	_ "resk"
 	"resk/infra"
+
+	_ "net/http/pprof"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/tietang/props/ini"
+	"github.com/tietang/props/kvs"
 )
 
 func main() {
+	//通过HTTP服务来开启运行时性能剖析
+	go func() {
+		log.Info(http.ListenAndServe(":6060", nil))
+	}()
 	file := kvs.GetCurrentFilePath("config.ini", 1)
 	conf := ini.NewIniFileConfigSource(file)
 	app := infra.New(conf)
